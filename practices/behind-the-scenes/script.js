7d21596e7cd3a4d75d1b24d80ffa2a404bf213e6
HelmiRonawan <ahmadhelmi2804@gmail.com>
2026-01-20 22:47:59 +0800

    Practice how hoisting and TDZ work

--- practices/behind-the-scenes/script.js
+++ practices/behind-the-scenes/script.js
@@ -1,5 +1,6 @@
 'use strict';
-
+// Scope
+console.log('===Scope===');
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -35,3 +36,48 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+
+// Hoisting and TDZ
+console.log('===Hoisting and TDZ===');
+//Variables
+console.log(me);
+//console.log(job);
+//console.log(year);
+
+var me = 'Jonas';
+let job = 'Teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2,3));
+//console.log(addExpr(2,3));
+console.log(addArrow);
+//console.log(addArrow(2,3));
+function addDecl(a,b){
+  return a + b;
+}
+
+const addExpr = function(a,b){
+  return a + b;
+}
+
+var addArrow = (a,b) => a + b;
+
+// Example
+console.log(numProducts);
+if (!numProducts) deleteShopping();
+
+var numProducts = 10;
+
+function deleteShopping(){
+  console.log('All products deleted');
+}
+
+var x =1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
+
