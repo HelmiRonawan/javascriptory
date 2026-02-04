b1182b9603f4001f8441dc135cd5ef296eb89dd3
HelmiRonawan <ahmadhelmi2804@gmail.com>
2026-02-04 20:28:58 +0700

    Football bet2

--- challenges/football-betting.js
+++ challenges/football-betting.js
@@ -40,6 +40,7 @@ const game = {
   },
 };
 
+/*
 const [players1, players2] = game.players 
 console.log(players1, players2);
 
@@ -62,3 +63,34 @@ printGoals(...game.scored)
 
 game.odds.team1 < game.odds.team2 && console.log('Team 1 is more likely to win!')
 game.odds.team1 > game.odds.team2 && console.log('Team 2 is more likely to win!')
+*/
+
+// 1
+for(const [goal,player] of game.scored.entries()){
+  console.log(`Goal ${goal+1}: ${player}`);
+}
+
+// 2
+let sum = 0;
+const oddValues = Object.values(game.odds)
+for(const odd of oddValues){
+  sum+=odd;
+}
+const average = sum / oddValues.length
+console.log(average);
+
+// 3
+for (const [key, value] of Object.entries(game.odds)){
+  const teamStr = key === 'x' ? 'draw': `victory ${game[key]}`;
+  console.log(`Odd of ${teamStr}: ${value}`);
+  // console.log(`Odd of${game?.[key] && ' victory' || ''} ${game?.[key] || 'draw'}: ${value}`);
+}
+
+// 4
+const scorers = {}
+for(const player of game.scored){
+  // console.log(typeof scorers[player]);
+  scorers[player] ? scorers[player]++: scorers[player] = 1
+}
+
+console.log(scorers);
